2aac16cebd872b1c8cf3177ba78eaa8958e00073
Luan Nguyen <nguyenhuuluan434@gmail.com>
2018-10-29 15:15:50 +0700

    create project

---
+++ src/main/java/com/luannh/ExampleBuild/App.java
@@ -0,0 +1,28 @@
+package com.luannh.ExampleBuild;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		App app = new App();
+		System.out.println("Begin app!");
+		app.loadProperties("config.properties").forEach((k, v) -> System.out.println(k + ":" + v));
+
+	}
+
+	public Properties loadProperties(String path) {
+		Properties prop = new Properties();
+		try (InputStream stream = getClass().getClassLoader().getResourceAsStream(path)) {
+			prop.load(stream);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println("unable log properties file " + path);
+		}
+		return prop;
+	}
+}
